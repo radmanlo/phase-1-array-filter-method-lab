b96fc19f60747988fc2b511cbd1b9b7edfc96b8e
radmanlo <radman.lotfiazar@gmail.com>
2023-03-25 13:03:41 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution here
+
+function findMatching(drivers, name){
+    const findByName = []
+    drivers.filter((driver) => {
+        if (driver.toLowerCase() === name.toLowerCase())
+            findByName.push(driver)
+    })
+    return findByName
+}
+
+function fuzzyMatch( drivers, character){
+    const findByChar = []
+    drivers.filter( (driver) => {
+        let chars = ""
+        for (let i = 0; i < character.length; i++){
+            chars += driver.charAt(i)
+        }
+        if (character.toLowerCase() === chars.toLowerCase())
+            findByChar.push(driver)
+    } )
+    return findByChar
+}
+
+function matchName( drivers, name){
+    const findByName = []
+    drivers.filter((driver) => {
+        if (driver.name.toLowerCase() === name.toLowerCase())
+            findByName.push(driver)
+    })
+    return findByName
+}
+
