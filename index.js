220dd37fb6f48785489cb6998db62828d66f0cc7
radmanlo <radman.lotfiazar@gmail.com>
2023-03-25 14:07:10 +0300

    Complete Assignment with last version

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Code your solution here
 
 function findMatching(drivers, name){
-    return drivers.filter( driver => {
-        return driver.toLowerCase() === name.toLowerCase()
-    })
+    return drivers.filter( driver => driver.toLowerCase() === name.toLowerCase())
 }
 
 function fuzzyMatch( drivers, character){
@@ -17,11 +15,10 @@ function fuzzyMatch( drivers, character){
 }
 
 function matchName( drivers, name){
-    return drivers.filter( driver => {
-        return driver.name.toLowerCase() === name.toLowerCase()
-    })
+    return drivers.filter( driver => driver.name.toLowerCase() === name.toLowerCase())
 }
 
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-console.log( fuzzyMatch( drivers, "sa"))
+console.log( findMatching( drivers, "bobby"))
+console.log(drivers)
 
